a7a617005dfbf8ff1570c267c9e74d2eb9925580
carlosmcnt <neto.mossel12@gmail.com>
2022-06-17 18:11:16 -0300

    atualiza Pessoa para classe abstrata

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,16 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Carlos"
-    var cpf: String = "123.456.789-10"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main() {
-    val carlos = Pessoa()
-    println(carlos.pessoaInfo())
-}
+abstract class Pessoa(
+    val nome: String,
+    val cpf: String,
+)
