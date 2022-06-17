97428aa63cdc15f9ff531a525ffcbedaaa4f909f
carlosmcnt <neto.mossel12@gmail.com>
2022-06-17 19:20:44 -0300

    atualiza Funcionario

--- src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
@@ -1,11 +1,15 @@
 package one.digitalinnovation.digionebank
 
-import java.math.BigDecimal
-
 abstract class Funcionario(
     nome: String,
     cpf: String,
-    val salario: BigDecimal,
+    val salario: Double
 ) : Pessoa(nome, cpf) {
-    abstract fun calculaAuxilio()
+    protected abstract fun calculaAuxilio(): Double
+    override fun toString(): String = """
+        Nome: $nome
+        Cpf: $cpf
+        Salario: $salario
+        Auxilio: ${calculaAuxilio()}
+    """.trimIndent()
 }
